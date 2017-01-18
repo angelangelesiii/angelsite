a6983220a0f62f261d0c0dffaea8d820a6a12dd8
Angel Angeles <angelangelesiii@gmail.com>
2017-01-18 08:35:02 +0800

    Added underscores template

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Angel_Angeles_2
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'angelangeles2' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'angelangeles2' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'angelangeles2' ), 'angelangeles2', '<a href="https://automattic.com/" rel="designer">Angel Angeles III</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
