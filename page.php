f4c2193bd895c12c148f4a901db971ac66ca8249
Angel Angeles <angelangelesiii@gmail.com>
2017-01-21 18:51:59 +0800

    Fixed spacings

--- page.php
+++ page.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			<div class="page-space"></div>
 			<section class="single-post-page">
 				<div class="wrapper">
 					<?php
